341d05294eba8caf81b7481fa54f8ae51d9042e1
Roger Davenport <roger.neil.davenport@gmail.com>
2017-12-14 10:52:40 -0800

    created funtionality for populating a form and then updating that record

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,6 +2,6 @@
 
 page('/', app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initFormPage(ctx));
-
 page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
+page('/books/update/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initUpdatePage));
 page();
