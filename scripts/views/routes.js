012c8317fb4571e8f943de5bf72cbe0a93eba3a9
Roger Davenport <roger.neil.davenport@gmail.com>
2017-12-14 13:21:31 -0800

    created adminview and logon auth

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -3,6 +3,7 @@
 page('/', app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initFormPage(ctx));
 page('/books/delete/:id', ctx => app.Book.delete(ctx));
+page('/admin', app.adminView.initAdminViewPage);
 page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 page('/books/update/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initUpdatePage));
 page();
