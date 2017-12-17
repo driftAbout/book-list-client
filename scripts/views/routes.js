1da747739472667ede13fd55cac889302b6e5589
Kevin Miller <kevin@driftabout.com>
2017-12-16 20:04:52 -0800

    test

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -20,7 +20,7 @@ page.base(baseURL);
 
 
 
-page('/', ctx => {console.log(ctx); page({click: false}); app.Book.fetchAll(app.bookView.initIndexPage)});
+page('/', app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initFormPage(ctx));
 page('/admin', app.adminView.initAdminViewPage);
 page('/search', () => app.bookView.initSearchFormPage(app.bookView.initSearchResultsPage));
@@ -30,10 +30,4 @@ page('/books/add/:id', ctx => app.Book.insertFromSearch(ctx));
 page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 page('/search-results/:id', ctx => app.bookView.initDetailPage(app.Book.all[ctx.params.id]));
 
-// page('/', (ctx) => {
-//   console.log(ctx);
-//   if (!ctx.init) app.Book.fetchAll(app.bookView.initIndexPage);
-// });
-
-//page({dispatch:false});
 page();
