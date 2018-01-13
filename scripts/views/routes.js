2c92af3216dbc99be0e001e3ae97f3abe4186e0f
Kevin Miller <kevin@driftabout.com>
2018-01-12 17:12:08 -0800

    debugging

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -53,6 +53,7 @@ var app = app || {};
       if(!callback) return;
       //if a route was found, set the history state
       //if the route was called from the popstate event, don't set th3 history state again 
+      console.log('window.location.pathname', window.location.pathname);
       if (window.location.pathname !== route && historyOpt) history.pushState( ctx, null, route);
       //if (window.location.pathname !== route) history.replaceState( ctx, null, route);
       //invoke the callback function with the object as an argument
